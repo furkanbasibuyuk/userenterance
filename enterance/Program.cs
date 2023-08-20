549d3ffb66c1511d4a3920fdacd00ae32e3aafd5
furkanbasibuyuk <frkn.bbyk@hotmail.com>
2023-08-20 19:06:16 +0300

    Add project files.

---
+++ enterance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enterance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int userEnteranceRight = 3;
+            while(true) 
+            {
+                Console.WriteLine("Please write your nickname");
+                string userNickName = Console.ReadLine();
+
+                Console.WriteLine("Please enter your password");
+                string userPassword = Console.ReadLine();   
+
+                if (userNickName == "furkan" && userPassword == "1234") 
+                {
+                    Console.WriteLine("Congratulations you entered the system succesfully!!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your user name or user password is wrong! Try again.");
+
+                    if(userEnteranceRight > 0)
+                    {
+                        userEnteranceRight -= 1;    
+                    }
+                    if(userEnteranceRight == 0)
+                    {
+                        Console.WriteLine("Your enterance rights are done! You have no more rigth to enter the system");
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
